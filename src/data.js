4c83b93ec3ee0baf3769a809852a59a8b9639242
Aishmurali <125116839+Aishmurali@users.noreply.github.com>
2023-02-18 13:26:54 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,260 @@
+export const storeProducts = [
+    {
+      id: 1,
+      title: "Google Pixel - Black",
+      img: "img/product-1.png",
+      price: 59990,
+      company: "GOOGLE",
+      info:
+        "Google Pixel is a brand of portable consumer electronic devices developed by Google that run either ChromeOS or the Android operating system. The main line of Pixel products consist of Android-powered smartphones, which have been produced since October 2016 as the replacement of the older Nexus, and of which the Pixel 7 is the current model. The Pixel brand also includes laptop and tablet computers, as well as several accessories, and was originally introduced in February 2013 with the Chromebook Pixel.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "Samsung S7",
+      img: "img/product-2.png",
+      price: 19999,
+      company: "SAMSUNG",
+      info:
+        "Samsung Galaxy is Samsung Electronics' flagship line of Android smartphones and tablets. The original Samsung Galaxy smartphone debuted in 2009, and the line's first tablet, Samsung Galaxy Tab, came out the following year. Samsung Galaxy also includes internet of things (IoT) devices, such as smartwatches and Bluetooth earbuds.All Samsung Galaxy phones and other devices, including the company's Chromebook laptops, offer touchscreen capabilities and device pairing, which lets customers use multiple Samsung Galaxy devices at once. Samsung Galaxy devices are available for Verizon, AT&T, T-Mobile and other cellular carriers. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "HTC 10 - Black",
+      img: "img/product-3.png",
+      price: 35000,
+      company: "htc",
+      info:
+        "Founded in 1997 as a laptop manufacturer, Taiwan-based HTC soon began making smartphones based on Windows Mobile and Brew. It released the first commercial Android smartphone, the HTC Dream, in 2008, and is today a manufacturer of both Android and Windows based smartphones. HTC re-entered the tablet market with the Google Nexus 9 in 2014. HTC's latest mobile launch is the Wildfire E Plus. The mobile was launched in October 2022. The phone comes with a 6.52-inch touchscreen display with a resolution of 720 pixels by 1600 pixels.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      title: "HTC 10 - White",
+      img: "img/product-4.png",
+      price: 30000,
+      company: "htc",
+      info:
+        "Founded in 1997 as a laptop manufacturer, Taiwan-based HTC soon began making smartphones based on Windows Mobile and Brew. It released the first commercial Android smartphone, the HTC Dream, in 2008, and is today a manufacturer of both Android and Windows based smartphones. HTC re-entered the tablet market with the Google Nexus 9 in 2014. HTC's latest mobile launch is the Wildfire E Plus. The mobile was launched in October 2022. The phone comes with a 6.52-inch touchscreen display with a resolution of 720 pixels by 1600 pixels.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      title: "HTC Desire 626s",
+      img: "img/product-5.png",
+      price: 14999,
+      company: "htc",
+      info:
+        "Founded in 1997 as a laptop manufacturer, Taiwan-based HTC soon began making smartphones based on Windows Mobile and Brew. It released the first commercial Android smartphone, the HTC Dream, in 2008, and is today a manufacturer of both Android and Windows based smartphones. HTC re-entered the tablet market with the Google Nexus 9 in 2014. HTC's latest mobile launch is the Wildfire E Plus. The mobile was launched in October 2022. The phone comes with a 6.52-inch touchscreen display with a resolution of 720 pixels by 1600 pixels.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      title: "Vintage Iphone",
+      img: "img/product-6.png",
+      price: 28999,
+      company: "apple",
+      info:
+        "As per Apple, products are considered vintage when the company has stopped distributing them for sale more than 5 and less than 7 years ago.Interestingly, iPhone 6 Plus and iPhone 6s are already in the Vintage product category. iPhone 6 is the latest to join the list. Both the smartphones were launched in September 2014. The devices were discontinued in 2019. Apple iPhone 6 has a 4.7-inch Retina HD display. The phone was launched with iOS8 and got software updates till iOS12. The phone was not eligible for the iOS 13. However, Apple continued to offer security patches for the devices.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      title: "Iphone 7",
+      img: "img/product-7.png",
+      price: 13000,
+      company: "apple",
+      info:
+        "Apple iPhone is a series of smartphones based on the company’s proprietary operating system iOS. The first iPhone, announced in 2007, featured a touch screen input-based user interface, camera, and multimedia playback capabilities. The phone was later upgraded to support internet connectivity, which has since become one of the core capabilities of the iPhone.Apple entered into the big-screen smartphone category with the launch of the iPhone 6-series, which had a 4.7-inch and a 5.5-inch screen models. The series was later upgraded to 6S-series with hardware improvement and 3D pressure-sensitive touch input. At the same time, Apple also introduced the iPhone SE, which resembled the iPhone 5s but had the hardware of the 6S.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 8,
+      title: "Nokia 2780",
+      img: "img/product-8.png",
+      price: 4999,
+      company: "Nokia",
+      info:
+        "Nokia smartphones bring together sleek Nordic design with the latest Android features. They’re affordable, reliable, and part of a growing push towards more sustainable practices. Simply put, a Nokia phone is something you can just feel good about.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 9,
+      title: "Nokia 110",
+      img: "img/product-9.png",
+      price: 2990,
+      company: "Nokia",
+      info:
+        "Nokia smartphones bring together sleek Nordic design with the latest Android features. They’re affordable, reliable, and part of a growing push towards more sustainable practices. Simply put, a Nokia phone is something you can just feel good about.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 10,
+      title: "RedmiNote 10 Lite",
+      img: "img/product-10.png",
+      price: 2990,
+      company: "Xiaomi",
+      info:
+        "Redmi phones are some of the most popular Android devices on the market, and for good reason. They offer a great value for money, with a wide range of features and an attractive design. If you're looking for a Redmi phone, you're in the right place. Here, you'll find a Redmi phones list that are currently available and will available soon. Whether you're looking for the latest Redmi 2022 model or something from an earlier year, we've got you covered. So, what are you waiting for? Take a look and find the perfect Redmi phone for you.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 11,
+      title: "OPPO Reno8 T 5G",
+      img: "img/product-11.png",
+      price: 35990,
+      company: "BBK Electronics Corporation",
+      info:
+        "The founding team of OPPO has extensive experience in electronic industry. When the team left BBK Company and founded the new completely independent brand - OPPO, they adhered to the belief of creating great products. In the fierce competing market, OPPO gradually became one of the Top-5 mobile phone brands in the world and emerged in the IoT products.Today, OPPO produces mobile phones (including Reno series), headphones, watches, tablets and other smart devices. OPPO also developed ColorOS operating system. With an innovative, open and elegant software-hardware integrated experience, OPPO is committed to creating a beautiful and intelligent life for users all over the world.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 12,
+      title: "Vivo T1",
+      img: "img/product-12.png",
+      price: 14990,
+      company: " BBK Technologie",
+      info:
+        "Vivo develops dynamic and stylish products for passionate young people. It develops and manufactures smartphones, smartphone accessories, software, and online services. Founded in 2009, Vivo has quickly expanded into markets in India and South East Asia. At Vivo we are dedicated to the pursuit of perfection and continuously creating surprises for users through constant innovation. Pioneering the use of Hi-Fi audio chips in smartphones, Vivo created the very first smartphone with a dedicated Hi-Fi chip, the X1. Since then Vivo has remained committed to providing the very best possible audio experience to customers. Our product’s camera also represents one of our most important competitive advantages. Photography is a way for people to express themselves and share joy with others. Through our smartphones, we offer users a professional-grade photography solution, taking mobile photography to the level of an art form, witnessing and recording all of life’s beautiful moments. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 13,
+      title: "Realme 10 Pro Plus",
+      img: "img/product-13.png",
+      price: 24990,
+      company: "BBK Electronics Corporation",
+      info:
+        "realme has been devoted to empowering youth to be more daring through leap-forward technology and design. Upon the establishment of realme in 2018, we have never stopped outdoing ourselves in terms of branding, product performance, design, quality and customer service. Applying next-level technologies on smartphones and AIoT products, we are able to provide next-level experience to our consumers. Support from our fans had inspired us to become the smartphone brand to achieve shipment of 100 million in just three years in 2021, leading us to rank among mainstream smartphone brands rapidly.realme became one of the first smartphone manufacturers equipped with Qualcomm Snapdragon 865 5G chips. realme launched its first 5G flagship smartphone, realme X50 Pro 5G. realme announced that it had fully entered the 5G era..",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+
+
+    {
+      id: 14,
+      title: "Dell Computers",
+      img: "img/product-14.png",
+      price: 60990,
+      company: "Dell Inc",
+      info:
+        "Dell Inc. Dell sells personal computers (PCs), servers, data storage devices, network switches, software, computer peripherals, HDTVs, cameras, printers, and electronics built by other manufacturers. The company is known for how it manages its supply chain and electronic commerce.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 15,
+      title: "Hp Computers",
+      img: "img/product-15.png",
+      price: 62000,
+      company: "Hewlett-Packard Company",
+      info:
+        "HP desktop computers are a popular choice for home and office use. They are reliable, affordable, and has a wide range of models to choose from. Browse the top-ranked list of HP desktops below along with associated reviews and opinions.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 16,
+      title: "Lenovo Thinkpad Laptop",
+      img: "img/product-16.png",
+      price: 64000,
+      company: "Lenovo Group Limited",
+      info:
+        "Lenovo is one of the world's leading personal technology companies, producing innovative PCs and mobile internet devices. Now, #286 on fortune 500 list, Lenovo is the world's largest PC vendor and fourth largest smartphone company.Lenovo has grown faster than the market for more than four years because our exceptionally engineered products are meant for those who do. Our customers use innovation to bring progress to the world, and we design and build our products with this in mind, taking inspiration from the drive and imagination of the people who make things happen. Our technology helps those who do, do better.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 17,
+      title: "Hp Laptop",
+      img: "img/product-17.png",
+      price: 42000,
+      company: "Hewlett-Packard Company",
+      info:
+        "HP Laptops are a popular choice for home and office use. They are reliable, affordable, and has a wide range of models to choose from. Browse the top-ranked list of HP desktops below along with associated reviews and opinions.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 18,
+      title: "Dell Laptop",
+      img: "img/product-18.png",
+      price: 35000,
+      company: "Dell Inc",
+      info:
+        "There are several reasons Dell's laptops are great, but chief among them is the fact that the best Dell laptops always feature excellent hardware specs. The latest devices from the company feature powerful and efficient 12th Gen Intel CPUs, cutting-edge DDR5 RAM, and an abundance of speedy SSD storage.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 19,
+      title: "Apple MAC Book Laptops",
+      img: "img/product-19.png",
+      price: 83000,
+      company: "Apple Inc",
+      info:
+        "The MacBook is a brand of Mac notebook computers designed and marketed by Apple Inc. that use Apple's macOS operating system since 2006. It replaced the PowerBook and iBook brands during the Mac transition to Intel processors, announced in 2005.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 20,
+      title: "Acer Laptop",
+      img: "img/product-20.png",
+      price: 21000,
+      company: "Acer Inc. ",
+      info:
+        " Acer has a great reputation and is one of the leading laptop brands. It offers a wide variety of options, and arguably produces more budget-friendly laptops than most other manufacturers. However, it rarely launches a laptop with enough quality to rival the likes of the Dell XPS 13 or MacBook Air",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+
+    
+  ];
+  
+  export const detailProduct = {
+    id: 1,
+      title: "Google Pixel - Black",
+      img: "img/product-1.png",
+      price: 59990,
+      company: "GOOGLE",
+      info:
+        "Google Pixel is a brand of portable consumer electronic devices developed by Google that run either ChromeOS or the Android operating system. The main line of Pixel products consist of Android-powered smartphones, which have been produced since October 2016 as the replacement of the older Nexus, and of which the Pixel 7 is the current model. The Pixel brand also includes laptop and tablet computers, as well as several accessories, and was originally introduced in February 2013 with the Chromebook Pixel.",
+      inCart: false,
+      count: 0,
+      total: 0
+  };
+  
